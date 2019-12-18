89ab5457ca1405eddf5bf32787dce48d8a5f8484
Menaka HN <menakahn@gmail.com>
2019-12-18 10:15:20 +0000

    spring5 web-app

---
+++ src/test/java/com/menaka/Spring5WebAppApplicationTests.java
@@ -0,0 +1,15 @@
+package com.menaka;
+
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Spring5WebAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
